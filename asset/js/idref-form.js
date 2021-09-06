af66c163b4220429f418620cf63b8f958bb41b59
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-09-06 00:00:00 +0000

    Normalisation de l’indentation.

--- asset/js/idref-form.js
+++ asset/js/idref-form.js
@@ -3,14 +3,14 @@
 //Voir : http://documentation.abes.fr/aideidrefdeveloppeur/ch04.html
 
 var proxy;
-var idAutorite="";
-var remoteClientExist=false;
+var idAutorite = "";
+var remoteClientExist = false;
 var oFrame;
-var idrefinit=false;
+var idrefinit = false;
 
 var serializer = {
 
-    stringify: function(data){
+    stringify: function(data) {
         var message = "";
         for (var key in data) {
             if (data.hasOwnProperty(key)) {
@@ -20,7 +20,7 @@ var serializer = {
         return message.substring(0, message.length - 1);
     },
 
-    parse: function(message){
+    parse: function(message) {
         var data = {};
         var d = message.split("&");
         var pair, key, value;
@@ -34,89 +34,88 @@ var serializer = {
     }
 };
 
-function envoiClient(index1,index1Value,index2,index2Value,filtre1,filtre1Value,filtre2,filtre2Value,zones){
+function envoiClient(index1, index1Value, index2, index2Value, filtre1, filtre1Value, filtre2, filtre2Value, zones) {
 
-    index1Value = index1Value.replace(/'/g,"\\\'");
-	// a commenter pour votre application
-	$('#resultat').html("");
-	$('#resultat').hide();
+    index1Value = index1Value.replace(/'/g, "\\\'");
+    // a commenter pour votre application
+    $('#resultat').html("");
+    $('#resultat').hide();
 
-    if (initClient()==0) {};
+    if (initClient() == 0) {
+    };
     oFrame = document.getElementById("popupFrame");
-    if (!idrefinit){
+    if (!idrefinit) {
         oFrame.contentWindow.postMessage(serializer.stringify({Init:"true"}), "*");
-        idrefinit=false;
+        // TODO Toujours false?
+        idrefinit = false;
     }
-	//TODO : il faut mettre le nom de votre application cliente à la place de la valeur : NomDeVotreApplication
-    try{
-        if (zones!=null)
-            eval('oFrame.contentWindow.postMessage(serializer.stringify({Index1:\''+index1+'\',Index1Value:\''+index1Value+'\',Index2:\''+index2+'\',Index2Value:\''+index2Value+'\',Filtre1:\''+filtre1+"/"+filtre1Value+'\',Filtre2:\''+filtre2+"/"+filtre2Value+'\','+zones+',fromApp:\'NomDeVotreApplication\',AutoClick:\'false\'}), "*"); ');
-        if (filtre2!=null)
-            eval('oFrame.contentWindow.postMessage(serializer.stringify({Index1:\''+index1+'\',Index1Value:\''+index1Value+'\',Index2:\''+index2+'\',Index2Value:\''+index2Value+'\',Filtre1:\''+filtre1+"/"+filtre1Value+'\',Filtre2:\''+filtre2+"/"+filtre2Value+'\',fromApp:\'NomDeVotreApplication\',AutoClick:\'false\'}), "*"); ');
-        else if (filtre1!=null)
-            eval('oFrame.contentWindow.postMessage(serializer.stringify({Index1:\''+index1+'\',Index1Value:\''+index1Value+'\',Index2:\''+index2+'\',Index2Value:\''+index2Value+'\',Filtre1:\''+filtre1+"/"+filtre1Value+'\',fromApp:\'NomDeVotreApplication\',AutoClick:\'false\'}), "*"); ');
-        else if (index2!=null)
-            eval('oFrame.contentWindow.postMessage(serializer.stringify({Index1:\''+index1+'\',Index1Value:\''+index1Value+'\',Index2:\''+index2+'\',Index2Value:\''+index2Value+'\',fromApp:\'NomDeVotreApplication\',AutoClick:\'false\'}), "*"); ');
+    //TODO : il faut mettre le nom de votre application cliente à la place de la valeur : NomDeVotreApplication
+    try {
+        if (zones != null)
+            eval('oFrame.contentWindow.postMessage(serializer.stringify({Index1:\'' + index1 + '\',Index1Value:\'' + index1Value + '\',Index2:\'' + index2 + '\',Index2Value:\'' + index2Value + '\',Filtre1:\'' + filtre1 + "/" + filtre1Value + '\',Filtre2:\'' + filtre2 + "/" + filtre2Value + '\',' + zones + ',fromApp:\'NomDeVotreApplication\',AutoClick:\'false\'}), "*"); ');
+        if (filtre2 != null)
+            eval('oFrame.contentWindow.postMessage(serializer.stringify({Index1:\'' + index1 + '\',Index1Value:\'' + index1Value + '\',Index2:\'' + index2 + '\',Index2Value:\'' + index2Value + '\',Filtre1:\'' + filtre1 + "/" + filtre1Value + '\',Filtre2:\'' + filtre2 + "/" + filtre2Value + '\',fromApp:\'NomDeVotreApplication\',AutoClick:\'false\'}), "*"); ');
+        else if (filtre1 != null)
+            eval('oFrame.contentWindow.postMessage(serializer.stringify({Index1:\'' + index1 + '\',Index1Value:\'' + index1Value + '\',Index2:\'' + index2 + '\',Index2Value:\'' + index2Value + '\',Filtre1:\'' + filtre1 + "/" + filtre1Value + '\',fromApp:\'NomDeVotreApplication\',AutoClick:\'false\'}), "*"); ');
+        else if (index2 != null)
+            eval('oFrame.contentWindow.postMessage(serializer.stringify({Index1:\'' + index1 + '\',Index1Value:\'' + index1Value + '\',Index2:\'' + index2 + '\',Index2Value:\'' + index2Value + '\',fromApp:\'NomDeVotreApplication\',AutoClick:\'false\'}), "*"); ');
         else
-            eval('oFrame.contentWindow.postMessage(serializer.stringify({Index1:\''+index1+'\',Index1Value:\''+index1Value+'\',fromApp:\'NomDeVotreApplication\',AutoClick:\'false\'}), "*"); ');
+            eval('oFrame.contentWindow.postMessage(serializer.stringify({Index1:\'' + index1 + '\',Index1Value:\'' + index1Value + '\',fromApp:\'NomDeVotreApplication\',AutoClick:\'false\'}), "*"); ');
     }
-    catch(e){
-            alert("oFrame.contentWindow Failed? " + e);
+    catch(e) {
+        alert("oFrame.contentWindow Failed? " + e);
     }
 }
 
-function initClient(){
-	
-	//Rend la fenêtre deplacable
-	$("#popupContainer").draggable();
-	
-    if (remoteClientExist){
-        showPopWin("", screen.width*0.7, screen.height*0.6, null);
+function initClient() {
+    //Rend la fenêtre deplacable
+    $("#popupContainer").draggable();
+
+    if (remoteClientExist) {
+        showPopWin("", screen.width * 0.7, screen.height * 0.6, null);
         return 0;
     }
 
-    showPopWin("", screen.width*0.7, screen.height*0.6, null);
-    remoteClientExist=true;
-    if (document.addEventListener){
-        window.addEventListener("message", function(e){
+    showPopWin("", screen.width * 0.7, screen.height * 0.6, null);
+    remoteClientExist = true;
+    if (document.addEventListener) {
+        window.addEventListener("message", function(e) {
             traiteResultat(e);
         });
     }
     else {
-        window.attachEvent('onmessage', function(e){
+        window.attachEvent('onmessage', function(e) {
             traiteResultat(e);
         });
     }
     return 0;
 }
 
-function traiteResultat(e){
-//partie à adapter pour votre client
-	var data = serializer.parse(e.data);
+function traiteResultat(e) {
+    //partie à adapter pour votre client
+    var data = serializer.parse(e.data);
 
-	if (data["g"]!=null){
-		var resHtml = "<ul>";
-		resHtml += "<li>data['a'] : "+data['a']+"</li>";
-		resHtml += "<li>data['b'] : "+data['b']+"</li>";
-		resHtml += "<li>data['c'] : "+data['c']+"</li>";
-		resHtml += "<li>data['d'] : "+data['d']+"</li>";
-		resHtml += "<li>data['e'] : "+data['e']+"</li>";
-		resHtml += "<li>data['f'] : "+escapeHtml(data['f'])+"</li>";
-		resHtml += "<li>data['g'] : "+data['g']+"</li>";
-		resHtml += "</ul>";
-		$('#resultat').html(resHtml);
-		$('#resultat').show();
-		hidePopWin(null);
-	}
+    if (data["g"] != null) {
+        var resHtml = "<ul>";
+        resHtml += "<li>data['a'] : " + data['a'] + "</li>";
+        resHtml += "<li>data['b'] : " + data['b'] + "</li>";
+        resHtml += "<li>data['c'] : " + data['c'] + "</li>";
+        resHtml += "<li>data['d'] : " + data['d'] + "</li>";
+        resHtml += "<li>data['e'] : " + data['e'] + "</li>";
+        resHtml += "<li>data['f'] : " + escapeHtml(data['f']) + "</li>";
+        resHtml += "<li>data['g'] : " + data['g'] + "</li>";
+        resHtml += "</ul>";
+        $('#resultat').html(resHtml);
+        $('#resultat').show();
+        hidePopWin(null);
+    }
 }
 
 function escapeHtml(texte) {
     return texte
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
- }
-
-
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
