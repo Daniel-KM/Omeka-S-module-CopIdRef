ceef8f4ee832e76404ae180f7c0b4f43818f1d8f
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-02-27 00:00:00 +0000

    Nettoyage du code.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -2,29 +2,28 @@
 
 namespace CopIdRef;
 
-use Omeka\Mvc\Controller\Plugin\Messenger;
 use Omeka\Stdlib\Message;
 
 /**
  * @var Module $this
- * @var \Laminas\ServiceManager\ServiceLocatorInterface $serviceLocator
+ * @var \Laminas\ServiceManager\ServiceLocatorInterface $services
  * @var string $newVersion
  * @var string $oldVersion
  *
+ * @var \Omeka\Api\Manager $api
+ * @var \Omeka\Settings\Settings $settings
  * @var \Doctrine\DBAL\Connection $connection
  * @var \Doctrine\ORM\EntityManager $entityManager
- * @var \Omeka\Settings\Settings $settings
- * @var \Omeka\Api\Manager $api
+ * @var \Omeka\Mvc\Controller\Plugin\Messenger $messenger
  */
-$services = $serviceLocator;
-$connection = $services->get('Omeka\Connection');
-$entityManager = $services->get('Omeka\EntityManager');
 $plugins = $services->get('ControllerPluginManager');
-// $config = $services->get('Config');
 $api = $plugins->get('api');
+$settings = $services->get('Omeka\Settings');
+$connection = $services->get('Omeka\Connection');
+$messenger = $plugins->get('messenger');
+$entityManager = $services->get('Omeka\EntityManager');
 
 if (version_compare($oldVersion, '3.3.0.6', '<')) {
-    $messenger = new Messenger();
     $message = new Message(
         'Une option permet désormais de limiter les types de ressources à chercher.' // @translate
     );
